f575c28c790473c25da7136d872c77d039907f67
Adrian Tan E-Herng <adriantan3921@gmail.com>
2025-06-12 15:24:15 +0800

    Missing State

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -33,16 +33,36 @@ const postsSlice = createSlice({
                 likes: 0,
                 comments: 0,
             }
+            state.forEach(post2 => console.log(post2))
             state.push(newPosts);
         },
         updatePost: (state, action) => {
+            console.log(state)
             const index = state.findIndex((post) => post.id === action.payload.id);
             state[index] = action.payload;
+
+        },
+
+        deletePost: (state, action) => {
+            console.log('>', action.payload.id)
+            state.forEach(post2 => console.log(post2))
+            state = [{
+                id: 2,
+                image: "https://picsum.photos/id/124/500/500",
+                description: "Post 2 description",
+                date: new Date().toISOString(),
+                likes: 30,
+                comments: 10,
+            }]
+            // const updatedPosts = state.filter((post) => post.id !== action.payload.id);
+            // state = updatedPosts
+            // console.log(action.payload.id)
+            // console.log('updatedPosts', JSON.parse(updatedPosts[0]))
 
         }
     }
 });
 
-export const { createPost, updatePost } = postsSlice.actions;
+export const { createPost, updatePost, deletePost } = postsSlice.actions;
 
 export default postsSlice.reducer;
